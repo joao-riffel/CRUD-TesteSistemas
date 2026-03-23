f3c79d8f5a6c98be14006a6a3e93131776e872f9
joao-riffel <joaovicenteriffel05@gmail.com>
2026-03-23 14:18:36 -0300

    Criado Cadastro.js
    Criado o JavaScript de Cadastro dos alunos

---
+++ public/js/cadastro.js
@@ -0,0 +1,12 @@
+async function cadastrar() {
+    const nome = document.getElementById('nome').value;
+    const idade = document.getElementById('idade').value;
+
+    await fetch('/api/usuarios', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nome, idade })
+    });
+
+    alert('Aluno cadastrado!');
+}
